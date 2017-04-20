e1eca6ffd46ae722032f1d920b1bd9780bd83d49
Johnathon Ludeman <jludeman@kabaminc.com>
2017-04-20 11:16:37 -0700

    add conversion of subdomain to variable

---
+++ Ludeblog/BaseModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Nancy;
+
+public class BaseModule : NancyModule {
+	public BaseModule() {
+		Before += ctx => {
+			String subdomainPattern = @"https?://(www[.])?(?<subdomain>\w+)[.]ludeman[.]com";
+			String url = ctx.Request.Url;
+
+			Match match = Regex.Match(url, subdomainPattern);
+			String subdomain = match.Groups["subdomain"].Value;
+
+			if (String.IsNullOrEmpty(subdomain)) {
+				Response response = new Response();
+				response.StatusCode = Nancy.HttpStatusCode.NotFound;
+
+				return response;
+			}
+
+			ctx.Parameters.subdomain = subdomain;
+			return null;
+		};
+	}
+}
