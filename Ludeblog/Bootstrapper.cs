e443c7879f80a7c8bb094cec562f2d6a63f7a49d
Johnathon Ludeman <jludeman@kabaminc.com>
2017-04-21 12:52:24 -0700

    add debug bootstrapper

---
+++ Ludeblog/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿#if DEBUG
+
+using System;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace Ludeblog {
+	public class CustomBootstrapper : DefaultNancyBootstrapper {
+		protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines) {
+			base.ApplicationStartup(container, pipelines);
+			StaticConfiguration.DisableErrorTraces = false;
+			Console.WriteLine("Bootstrapper enabled!!");
+		}
+	}
+}
+
+#endif
